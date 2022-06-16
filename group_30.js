fd0f1b93b3a17514c33f361bc96a4c2442c05087
Yuliya Adamchuk <Adamchuk1103@mail.ru>
2022-06-16 14:38:41 +0300

    remote.js

--- group_30.js
+++ group_30.js
@@ -1,29 +1,42 @@
+//1-3
 let item_1 = 5
 console.log(item_1)
 
+//4-6
 let item_2 = 3
 console.log(item_2)
 
+//7-9
 let item_3 = item_1 + item_2
 console.log(item_3)
 
+//10-12
 let item_4 = "Yolochka"
 console.log(item_4)
 
-console.log(item_3 * item_4)
-console.log(item_3 *item_4)
 
+//13-14
+console.log(item_3 + item_4)
+console.log(item_3 * item_4)
+
+//15-16
 let item_5 = item_3
 console.log(item_5)
 
+//17-21
 let item_6 = 15
-let item_6_type = item_6
-console.log("item_6 ==", typeof(item_6), "item_6_type ==", typeof(item_6_type))
+let item_6_type = typeof(item_6)
+console.log("item_6 == " , typeof item_6)
+console.log("item_6_type == " , typeof item_6_type)
+
+//22-25
+let item_7 = "item_6"
+let item_7_type = typeof(item_7)
+console.log("item_7 ==", typeof item_7, "item_7_type==", typeof item_7_type)
 
 
 
-
-
+//26-33
 
 let age_1 = 10;
 let age_2 = 18;
--- group_30_2.js
+++ group_30_2.js
@@ -1,27 +1,30 @@
+
 let age_1 = 10;
 let age_2 = 18;
 let age_3 = 60;
-checkAge(17);
-checkAge(18);
-checkAge(61);  
-const checkAge = function(age_1){
-if (age_1 < age_2) {
-     console.log("You don't have access cause your age is " + age_1 + " It’s less then");
+
+
+const checkAge = function(age){ 
+   
+ if (age < age_2) {
+     console.log("You don't have access cause your age is " + age + " It’s less then");
 }
-else  if (age_1 >= age_2 || age_1 < age_3) 
+   if (age >= age_2 || age < age_3) 
  {
  console.log("Welcome!");
  } 
- else if
- (age_1 > age_3) {
+   if
+ (age > age_3) {
 console.log( "Keep calm and look Culture channel");
- } else
+ } else 
  {
      console.log("Technical work");
  }
-
 }
- 
+
+checkAge(17);
+checkAge(18);
+checkAge(61);
 
 
 
